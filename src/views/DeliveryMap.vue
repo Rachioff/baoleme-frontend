c269e9652767d84bc10ef0c6025707ddaa988958
CrSjimo <crs_16423@outlook.com>
2025-06-05 14:41:59 +0800

    feat: 添加了配送地图

--- src/views/DeliveryMap.vue
+++ src/views/DeliveryMap.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import AMapLoader from "@amap/amap-jsapi-loader";
-import { onMounted } from "vue";
+import { onMounted, watch } from "vue";
 
 (window as any)._AMapSecurityConfig = {
     serviceHost: import.meta.env.VITE_AMAP_SERVICE,
@@ -23,17 +23,49 @@ onMounted(async () => {
     AMap = await AMapLoader.load({
         key: import.meta.env.VITE_AMAP_KEY,
         version: "2.0",
-        plugins: ["AMap.Scale"],
+        plugins: ["AMap.Scale", "AMap.Riding"],
     })
     map = new AMap.Map("container", {
         viewMode: '3D',
-        zoom: 11,
+        zoom: 13,
         center: [props.currentLongitude, props.currentLatitude],
     })
+    await updateRoute()
+})
+
+const updateRoute = () => {
+    return new Promise<void>((resolve, reject) => {
+        if (!map) {
+            resolve()
+            return
+        }
+        const start = new AMap.LngLat(props.startLongitude, props.startLatitude);
+        const end = new AMap.LngLat(props.endLongitude, props.endLatitude);
+        const riding = new AMap.Riding({
+            map: map,
+        });
+        const marker = new AMap.Marker({
+            position: new AMap.LngLat(props.currentLongitude, props.currentLatitude),
+        });
+        map.add(marker);
+        riding.search(start, end, (status: string, result: any) => {
+            if (status === 'complete') {
+
+            } else {
+                console.error('路线规划失败', result);
+                reject(new Error('路线规划失败'));
+            }
+            resolve()
+        });
+    })
+}
+
+watch(props, async () => {
+    await updateRoute()
 })
 
 
 </script>
 <template>
-    <div id="container"></div>
+    <div id="container" style="width: 100%; height: 200px;"></div>
 </template>
