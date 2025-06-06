1368b42f23239db0a6a0c47cf380676a5c644782
rachioff <15523001+rachioff@user.noreply.gitee.com>
2025-06-06 23:14:46 +0800

    Revert "添加了骑手接单页面"
    This reverts commit dd290144358778529cb70cbda24e0950aa89f82f.

--- src/router/index.ts
+++ src/router/index.ts
@@ -142,12 +142,6 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import('@/views/customer/order/Checkout.vue'),
     meta: { requiresAuth: true }
   },
-  {
-    path: '/rider/recommend',
-    name: 'Recommend',
-    component: () => import('@/views/rider/Recommend.vue'),
-    meta: { requiresAuth: true }
-  },
 ]
 
 const router = createRouter({
